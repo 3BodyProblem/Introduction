b03084f0e584fac79c485edebb1e131c49a566e4
BarryPaneer <barry.paneer@qq.com>
2017-06-07 00:53:58 +0800

    updates

--- Protocal/admin.py
+++ Protocal/admin.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from .models import *
 
 
+admin.site.site_header = "研发接口文档(Barry)"
 admin.site.register( DataSourceType, DataSourceTypeAdmin )
 admin.site.register( MarketsSupport, MarketsSupportAdmin )
 admin.site.register( DataType, DataTypeAdmin )
-admin.site.register( Quotation, QuotationAdmin )
+admin.site.register( Message, MessageAdmin )
+admin.site.register( FieldDefinition, FieldDefinitionAdmin )
 
 
 
--- Protocal/models.py
+++ Protocal/models.py
@@ -2,13 +2,17 @@ from django.contrib import admin
 from django.db import models
 
 
-
 class DataSourceType( models.Model ):
 	ID = models.AutoField( primary_key = True )
 	DataSourceName = models.CharField( max_length = 32 )
 	DataSourceDesc = models.CharField( max_length = 128 )
 
+	def __str__( self ):
+		return self.DataSourceName
+
 	class Meta:
+		verbose_name = '数据源'
+		verbose_name_plural = '数据源类型'
 		db_table = 'T_DataSource'
 
 
@@ -21,7 +25,12 @@ class MarketsSupport( models.Model ):
 	MkName = models.CharField( max_length = 32 )
 	DataSourceID = models.ForeignKey( DataSourceType )
 
+	def __str__( self ):
+		return self.MkName
+
 	class Meta:
+		verbose_name = '市场'
+		verbose_name_plural = '市场列表'
 		db_table = 'T_Markets'
 
 
@@ -35,7 +44,12 @@ class DataType( models.Model ):
 	TypeDesc = models.CharField( max_length = 128 )
 	TypeDefinition = models.CharField( max_length = 64 )
 
+	def __str__( self ):
+		return self.TypeName
+
 	class Meta:
+		verbose_name = '字段类型'
+		verbose_name_plural = '字段类型定义'
 		db_table = 'T_DataType'
 
 
@@ -47,23 +61,62 @@ class DataTypeAdmin( admin.ModelAdmin ):
 		]
 
 
-class Quotation( models.Model ):
+class Message( models.Model ):
+	ID = models.AutoField( primary_key = True )
+	MessageID = models.IntegerField( null = True, blank = False )
+	MessageName = models.CharField( max_length = 32 )
+	MessageDesc = models.CharField( max_length = 64 )
+	MarketID = models.ForeignKey( MarketsSupport, null = True )
+
+	def __str__( self ):
+		return self.MessageName
+
+	class Meta:
+		verbose_name = '消息描述'
+		verbose_name_plural = '消息描述列表'
+		db_table = 'T_Message'
+
+
+class MessageAdmin( admin.ModelAdmin ):
+	list_display = ( 'MessageID', 'MessageName', 'MessageDesc', 'MarketID' )
+	fieldsets = [
+			(None, {'fields':['MessageID', 'MessageName']}),
+			('Message Definition: ', {'fields':['MessageDesc', 'MarketID']})
+		]
+
+
+class FieldDefinition( models.Model ):
 	ID = models.AutoField( primary_key = True )
 	AttributeName = models.CharField( max_length = 20 )
 	AttributeType = models.ForeignKey( DataType, null = True )
 	AttributeDesc = models.CharField( max_length = 128 )
-	MarketID = models.ForeignKey( MarketsSupport, null = True )
+	MessageID = models.ForeignKey( Message, null = True )
+
+	def __str__( self ):
+		return self.AttributeName
 
 	class Meta:
-		db_table = 'T_Quotation'
+		verbose_name = '消息字段'
+		verbose_name_plural = '消息字段列表'
+		db_table = 'T_Field_Definition'
+		ordering=['ID']
 
 
-class QuotationAdmin( admin.ModelAdmin ):
-	list_display = ( 'AttributeName', 'AttributeType', 'AttributeDesc' )
+class FieldDefinitionAdmin( admin.ModelAdmin ):
+	list_display = ( 'AttributeName', 'AttributeType', 'AttributeDesc', 'MessageID' )
+	search_fields = ( 'MessageID', )
 	fieldsets = [
 			(None, {'fields':['AttributeName']}),
-			('Message Description:', {'fields':['AttributeType','AttributeDesc']})
+			('Message Description:', {'fields':['AttributeType','AttributeDesc','MessageID']})
 		]
+
+
+
+
+
+
+
+
 
 
 
